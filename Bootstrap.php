02d2f1fa4c5323d2d8700be9068359da3d62a81c
rafli1319 <120114275+rafli1319@users.noreply.github.com>
2022-12-08 22:54:52 +0700

    Add files via upload

---
+++ Bootstrap.php
@@ -0,0 +1,223 @@
+<!doctype html>
+<html lang="en">
+<style>
+.tengah{
+    margin-left:auto;margin-right:auto;display:block;width:400px
+} 
+</style>
+<div class="overflow-auto">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Profil 19SA1224</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+    <nav class="navbar navbar-expand-lg bg-secondary ">
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse  justify-content-center" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <style>
+      a {
+        color: black;
+      }
+
+      a:hover {
+        color: red;
+      }
+    </style>
+  </head>
+
+
+  <body>
+    <div class="h-100 p-5 bg-light rounded-3">
+      <img src="2.png" class="rounded mx-auto d-block shadow-lg p-1 mb-1 bg-body rounded-circle" height="300px" width="300px">
+      <h2 style="text-align: center; color:black;" class="fs-4">MUHAMMAD RAFLI ASSIDIQ</h2>
+
+      <div class="text-center">
+        <i class="bi bi-envelope-fill d-inline "></i>
+        <a href="mailto:raflia1319@gmail.com" target="_blank" style="text-decoration:none;">
+          <h6 class="d-inline ms-1 me-2 fw-light">raflia1319@gmail.com.com</h6>
+        </a>
+        <i class="bi bi-telephone-fill"></i>
+        <a href=" https://wa.me/+6289663035241" target="_blank" style="text-decoration:none;">
+          <h6 class="d-inline ms-1 me-2 fw-light">+6289663035241</h6>
+        </a>
+        <i class="bi bi-geo-alt-fill"></i>
+        <a href="https://goo.gl/maps/uS3pbm5pxqmGbjhP8" target="_blank" style="text-decoration:none;">
+          <h6 class="d-inline ms-1 fw-light">Ds.Tanjungmeru, Kec. Kutowinangun</h6>
+        </a>
+
+      </div>
+    </div>
+    <br />
+
+    <div class="row-cols-1 row-cols-md-1 g-4">
+        <div class="col">
+          <div class="card h-100 w-50 tengah" style="background:pink">
+            <img src="profile.png" class="card-img-top rounded mx-auto d-block" style="height: 200px; width: 200px;">
+            <div class="card-body">
+              <h5 class="card-title text-white"style="text-align: center;">Biodata</h5>
+              <p class="card-text text-white"style="text-align: center;">Berikut Biodata Saya</p>
+              <ul class="list-group list-group-flush mx-auto d-block" style="text-align: center;">
+                <li class="list-group-item text-truncate">Nama   : Muhammad Rafli Assidiq</li>
+                <li class="list-group-item text-truncate">Nim    : 19SA1281</li>
+                <li class="list-group-item text-truncate">Prodi  : Informatika</li>
+              </ul>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 2 second ago</small>
+            </div>
+          </div>
+        </div>
+        <br/>
+        <br/>
+        <div class="col">
+          <div class="card h-100 w-50 tengah" style="background:pink">
+            <img src="education.png" class="card-img-top rounded mx-auto d-block" style="height: 200px; width: 200px;">
+            <div class="card-body">
+              <h5 class="card-title text-white"style="text-align: center;">Akademik</h5>
+              <p class="card-text text-white"style="text-align: center;">Berikut Riwayat Akademik Saya</p>
+              <ul class="list-group list-group-flush"style="text-align: center;">
+                <li class="list-group-item text-truncate">SD Negeri Tanjungmeru</li>
+                <li class="list-group-item text-truncate">SMP Negeri 1 Kutowinangun</li>
+                <li class="list-group-item text-truncate">SMA Negeri 1 Kutowinangun</li>
+              </ul>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 2 days ago</small>
+            </div>
+          </div>
+        </div>
+        <br/>
+        <br/>
+        <div class="col">
+          <div class="card h-100 w-50 tengah" style="background:pink">
+            <img src="hobbies.png" class="card-img-top rounded mx-auto d-block" style="height: 200px; width: 200px;">
+            <div class="card-body">
+              <h5 class="card-title text-white"style="text-align: center;">Hobby</h5>
+              <p class="card-text text-white"style="text-align: center;">Berikut Hobby Saya</p>
+              <ul class="list-group list-group-flush" style="text-align: center;">
+                <li class="list-group-item text-truncate">Bulu Tangkis</li>
+                <li class="list-group-item text-truncate">Main Game</li>
+                <li class="list-group-item text-truncate">Editing Video</li>
+              </ul>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 2 days ago</small>
+            </div>
+          </div>
+        </div>
+      </div> 
+      </div>
+      <br/>
+      <br/>
+      <br/>
+    </section>
+
+    
+    <section id="popular-courses" class="courses">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Gambar Akademik</h2>
+          <p>Sekolah Tempat Saya Belajar</p>
+        </div>
+
+        <div class="row" data-aos="zoom-in" data-aos-delay="100">
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="course-item">
+              <img src="sd.jpg" class="img-fluid">
+              <div class="course-content">
+                <div class="d-flex justify-content-between align-items-center mb-3">
+                  <h4>SD Negeri Tanjungmeru</h4>
+                  <p class="price"></p>
+                </div>
+
+                
+              </div>
+            </div>
+          </div> 
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+            <div class="course-item">
+              <img src="smp.jpg" class="img-fluid">
+              <div class="course-content">
+                <div class="d-flex justify-content-between align-items-center mb-3">
+                  <h4>SMP Negeri 1 Kutowinangun</h4>
+                  <p class="price"></p>
+                </div>
+
+                
+              </div>
+            </div>
+          </div> 
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+            <div class="course-item">
+              <img src="sma.jpg" class="img-fluid">
+              <div class="course-content">
+                <div class="d-flex justify-content-between align-items-center mb-3">
+                  <h4>SMA Negeri 1 Kutowinangun</h4>
+                  <p class="price"></p>
+                </div>
+
+              </div>
+            </div>
+          </div> 
+
+        </div>
+
+        </div>
+
+      </div>
+    </section>
+    <br/>
+    <br/>
+      
+      
+      
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+  
+  <footer class="text-center text-lg-start" style="background:gray">
+      <div class="text-center p-2" style="color:white;">
+        © 2022 Copyright:
+      <a class="text-light bg-secondary" href="https://www.facebook.com/" target="_blank">19SA1281</a>
+    </div>
+  </footer>
+
+</div>
+
+</html>
